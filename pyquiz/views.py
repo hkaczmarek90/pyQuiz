c7446a45c213fdf61a3af6f1ecef5e1f7552a89e
AgADz <dzbanuszek.agnieszka@gmail.com>
2020-07-19 10:56:47 +0200

    add initial login form

--- pyquiz/views.py
+++ pyquiz/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 
 def home(request):
     return render(request, 'home.html')
+
+
+def profile(request):
+    return render(request, 'profile_side.html', {'user': request.user})
