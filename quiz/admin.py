af65344cc56ced1372edfd1ac78b76f3a2b41cb8
Kamil Kujawa <kaku.ex@boozt.com>
2020-07-19 15:09:50 +0200

    Add answer model

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from .models import Question, Quiz
+from .models import Question, Answer, Quiz
 
 admin.site.register(Question)
+admin.site.register(Answer)
 admin.site.register(Quiz)
-
