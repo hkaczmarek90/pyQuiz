f09d2a70cc7dee94a2f01de83420b6ea379d370b
AgADz <dzbanuszek.agnieszka@gmail.com>
2020-07-19 13:33:44 +0200

    add model: quiz

--- quiz/models.py
+++ quiz/models.py
@@ -8,3 +8,13 @@ class Question(models.Model):
 
     def __str__(self):
         return self.value
+
+
+class Quiz(models.Model):
+    name = models.TextField()
+    public = models.BooleanField()
+    description = models.TextField()
+    created_by = models.ForeignKey('auth.User', on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.name
