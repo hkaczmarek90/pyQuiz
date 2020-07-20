b215ac1b6599662a64c30f43991b38c2fdb0313f
Hubert Kaczmarek <h.kaczmarek90@gmail.com>
2020-07-20 20:39:32 +0200

    New branch for views

--- pyquiz/views.py
+++ pyquiz/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from pyquiz.quiz.models import Quiz
 
 
 def home(request):
@@ -7,3 +8,8 @@ def home(request):
 
 def profile(request):
     return render(request, 'profile_side.html', {'user': request.user})
+
+
+def quizzes(request):
+    quizzes = Quiz.objects.all()
+    return render(request, 'quizzes.html', {'quizzes': quizzes})
